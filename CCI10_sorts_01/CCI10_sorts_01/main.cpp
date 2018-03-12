961bf08a87ee7103885ac2de1206223428fedfc1
John Schar <johnschar2001@yahoo.com>
2018-03-12 08:30:55 -0700

    More sorts
    Different sorts for c++

--- CCI10_sorts_01/CCI10_sorts_01/main.cpp
+++ CCI10_sorts_01/CCI10_sorts_01/main.cpp
@@ -6,8 +6,28 @@
 #include <iostream>
 #include <stdlib.h>
 #include <time.h>
+#include <iomanip>
 
 using namespace std;
+
+/* Print the given array.*/
+void printArr(int *arr, int size);
+void bubbleSort(int *arr, int size);
+void insertionSort(int *arr, int size);
+void selectionSort(int *arr, int size);
+void mergeSort(int *arr, int size);
+void mergeSort(int *arr, int start, int end);
+void merge(int *arr, int start, int mid, int end);
+void quickSort(int *arr, int size);
+void quickSort(int *arr, int start, int end);
+int getMax(int *arr, int size);
+int getMin(int *arr, int size);
+void countSort(int *arr, int size);
+void radixSort(int *arr, int size);
+void countSort(int *arr, int size, int exp);
+void heapSort(int *arr, int size);
+void heapify(int *arr, int size, int i);
+void swap( int* a, int* b);
 
 /*
 	Bubble sort. O(n^2)
@@ -16,7 +36,7 @@ using namespace std;
 	Bubble sort is bad because of its slow execution time. We have to iterate through the entire list and compare
 	all elements for every element.
 */
-void bubbleSort(int *arr, int size){
+void bubbleSort(int *arr, int size) {
 	for(int i=0; i<size-1; i++){
 		for(int j=size-1; j>i; j--){
 			if(arr[j-1] > arr[j]){
@@ -74,12 +94,6 @@ void selectionSort(int *arr, int size){
 	}
 }
 
-
-
-void mergeSort(int*,int);
-void mergeSort(int*, int, int);
-void merge(int*, int, int, int);
-
 /*
 
 Merge sort. Space O(n), Time O(nlogn)
@@ -98,7 +112,6 @@ void mergeSort(int *arr, int size){
 	Merge sort helper.
 	Passes split array and passes array to be merged.
 */
-
 void mergeSort(int *arr, int start, int end){
 	if( start < end ){
 		int mid = (start+end)/2;
@@ -112,7 +125,6 @@ void mergeSort(int *arr, int start, int end){
 	merge
 	Merge two sorted arrays. Uses a temporary array to merge the sorted array.
 */
-
 void merge(int *arr, int start, int mid, int end){
 	int tSize = end-start+1;	// Temporary size.
 	int *tArr = new int[tSize](); // Temporary array to hold our sorted values.
@@ -142,7 +154,6 @@ void merge(int *arr, int start, int mid, int end){
 	Swap the pivot back with the wall, recurse and split the array.
 */
 
-void quickSort(int*,int,int);
 void quickSort(int *arr, int size){
 	if(size <= 1){ return; }
 	quickSort(arr,0,size-1);
@@ -172,18 +183,180 @@ void quickSort(int *arr, int start, int end){
 	quickSort(arr, wIndex, end);
 }
 
-void radixSort(int *arr, int size){
-
+/*
+	getMax
+	Return the largest value in an array.
+*/
+int getMax(int *arr, int size){
+	if(size <= 0){ return INT_MIN; }
+	if(size == 1){ return arr[0]; }
+	int max = arr[0];
+	for(int i=0;i<size; i++){
+		if(arr[i] > max){ max = arr[i]; }
+	}
+	return max;
 }
 
-void heapSort(int *arr, int size){
+/*
+	getMin
+	Return the smallest value in an array.
+*/
+int getMin(int *arr, int size){
+	if(size <= 0){ return INT_MIN; }
+	if(size == 1){ return arr[0]; }
+	int min = arr[0];
+	for(int i=0; i<size; i++){
+		if(arr[i] < min){ min = arr[i]; }
+	}
+	return min;
+}
 
+/*
+	Counting sort. Time O(n+k), Space O(n+k)
+
+	With counting sort we count all the numbers in the unsorted into another array. The count array is then added
+	together which then becomes a position array. Elements in the unsorted array are read again but are used to
+	look up it positions in the count array, sort of like a hash. Using the positions and the elements in the
+	unsorted array a new sorted arrary is made.
+
+	Points to be noted:
+	1. Counting sort is efficient if the range of input data is not significantly greater than the number
+	of objects to be sorted. Consider the situation where the input sequence is between range 1 to 10K
+	and the data is 10, 5, 10K, 5K.
+	2. It is not a comparison based sorting. It running time complexity is O(n) with space proportional to the range of data.
+	3. It is often used as a sub-routine to another sorting algorithm like radix sort.
+	4. Counting sort uses a partial hashing to count the occurrence of the data object in O(1).
+	5. Counting sort can be extended to work for negative inputs also.
+*/
+
+void countSort(int *arr, int size) {
+	if(size <= 1){ return; }
+	int low = getMin(arr, size);
+	int high = getMax(arr, size);
+	int cSize = high-low+1; // size of the count array.
+	int *cArr = new int[cSize](); // The count and position array.
+	int *sArr = new int[size](); // The array sorted.
+
+	for(int i=0; i<size; i++){
+		cArr[arr[i]-low]++;	// Count elements in the array.	
+	}
+
+	for(int i=0; i<cSize-1; i++){
+		cArr[i+1] = cArr[i] + cArr[i+1]; //Add up array.
+	}
+
+	for(int i=0; i<size; i++){ // Sort array.
+		sArr[cArr[arr[i]-low]-1] = arr[i]; 
+		cArr[arr[i]-low]--;
+	}
+
+	for(int i=0; i<size; i++){ // Copy elements from the sorted array into the in array.
+		arr[i] = sArr[i];
+	}
+}
+
+/*
+	Radix sort. 
+
+	Counting sort is a linear time sorting algorithm that sort in O(n+k) time when elements are in range from 1 to k. What if
+	the elements are in range from 1 to n2?	We can’t use counting sort because counting sort will take O(n2) which is worse
+	than comparison based sorting algorithms. Can we sort such an array in linear time?	Radix Sort is the answer. The idea
+	of Radix Sort is to do digit by digit sort starting from least significant digit to most significant digit. Radix sort
+	uses counting sort as a subroutine to sort.
+
+	What is the running time of Radix Sort?
+	Let there be d digits in input integers. Radix Sort takes O(d*(n+b)) time where b is the base for representing numbers,
+	for example, for decimal system, b is 10. What is the value of d? If k is the maximum possible value, then d would be
+	O(logb(k)). So overall time complexity is O((n+b) * logb(k)). Which looks more than the time complexity of comparison
+	based sorting algorithms for a large k. Let us first limit k. Let k <= nc where c is a constant. In that case, the
+	complexity becomes O(nLogb(n)). But it still doesn’t beat comparison based sorting algorithms. What if we make value of
+	b larger?. What should be the value of b to make the time complexity linear? If we set b as n, we get the time complexity
+	as O(n). In other words, we can sort an array of integers with range from 1 to nc if the numbers are represented in base
+	n (or every digit takes log2(n) bits).
+*/
+
+void radixSort(int *arr, int size){
+	int m = getMax(arr,size); // Largest has the most digits to step through.
+	for( int exp=1; m/exp>0; exp*=10){
+		countSort(arr, size, exp);
+	}
+}
+
+/*
+	Counting sort for radix.
+	exp is used to sort a digit at a specific place.
+*/
+
+void countSort(int *arr, int size, int exp) {
+	if (size <= 1) { return; }
+	int cSize = 10; // 0-9.
+	int *cArr = new int[cSize](); // The count and position array.
+	int *sArr = new int[size](); // The array sorted.
+
+	for (int i = 0; i<size; i++) {
+		cArr[(arr[i]/exp)%10]++;	// Count elements in the array.	
+	}
+
+	for (int i = 0; i<cSize - 1; i++) {
+		cArr[i + 1] += cArr[i]; //Add up array.
+	}
+
+	for (int i = size-1; i>=0; i--) { // Step backwads through the array.
+		sArr[cArr[(arr[i]/exp)%10]-1] = arr[i];
+		cArr[(arr[i]/exp)%10]--;
+	}
+
+	for (int i = 0; i<size; i++) { // Copy elements from the sorted array into the in array.
+		arr[i] = sArr[i];
+	}
+}
+
+/*
+	Max heapSort.
+
+	Time Complexity: Time complexity of heapify is O(Logn). Time complexity of createAndBuildHeap()
+	is O(n) and overall time complexity of Heap Sort is O(nLogn).
+
+	Manipulate an array so it resembles a max heap.
+*/
+void heapSort(int *arr, int size){
+	for(int i = size / 2 - 1; i >= 0; i--){
+		heapify(arr, size, i);
+	}
+}
+
+void heapify(int *arr, int size, int i){
+	int largest = i; // I is the temp root for the tree underneath it.
+	int l = 2*i+1; // Left child of the temp root.
+	int r = 2*i+2; // Right child of the temp root.
+
+	// If the left child is larger than the root then swap it.
+	if( l < size && arr[l] > arr[largest] ){
+		largest = l;
+	}
+
+	// If the right child is larger than the root then swap it.
+	if( r < size && arr[r] > arr[largest] ){
+		largest = r;
+	}
+
+	// If the largest changed then it won't be the root and we need to swap it.
+	if(largest != i){
+		swap(arr[i], arr[largest]);
+		heapify(arr, size, largest); // Largest is the index of the child we swapped so recurse down that way.
+	}
+}
+
+void swap(int *a, int *b){
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
 
 void printArr(int *arr, int size){
 	for(int i=0; i<size; i++){
-		cout << arr[i] << ' ';
+		cout << setw(4) << arr[i];
 	}
 	cout << endl;
 }
@@ -191,7 +364,6 @@ void printArr(int *arr, int size){
 int main(void){
 	
 	srand(time(nullptr));
-
 	int iArr[20] = {0};
 
 /*
@@ -214,17 +386,35 @@ int main(void){
 	printArr(iArr, 20);
 
 	for (int i = 0; i<20; i++) { iArr[i] = (rand() % 20) + 1; }
-	cout << "Selection sort." << endl;
+	cout << "Merge sort." << endl;
 	printArr(iArr, 20);
 	mergeSort(iArr, 20);
 	printArr(iArr, 20);
 
 	for (int i = 0; i<20; i++) { iArr[i] = (rand() % 20) + 1; }
-	cout << "Selection sort." << endl;
+	cout << "Quick sort." << endl;
 	printArr(iArr, 20);
 	quickSort(iArr, 20);
 	printArr(iArr, 20);
+
+	for (int i = 0; i<20; i++) { iArr[i] = (rand() % 40) + 1 - 20; }
+	cout << "Count sort." << endl;
+	printArr(iArr, 20);
+	countSort(iArr, 20);
+	printArr(iArr, 20);
+
+	for (int i = 0; i<20; i++) { iArr[i] = rand() % 20 + 1; }
+	cout << "Count sort." << endl;
+	printArr(iArr, 20);
+	radixSort(iArr, 20);
+	printArr(iArr, 20);
 */
+
+	for (int i = 0; i<20; i++) { iArr[i] = rand() % 20 + 1; }
+	cout << "Heap sort." << endl;
+	printArr(iArr, 20);
+	heapSort(iArr, 20);
+	printArr(iArr, 20);
 	
 	cout << "End of line." << endl;
 	cin.ignore();
